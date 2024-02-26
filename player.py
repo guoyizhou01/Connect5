1ab00407273a9d0e78de2b935f032146a0188eb4
guoyizhou01 <85047186+guoyizhou01@users.noreply.github.com>
2024-02-26 15:48:47 -0600

    updated

--- player.py
+++ player.py
@@ -1,6 +1,10 @@
 import numpy as np
+from anytree import Node, RenderTree
 
 rows, cols = 7, 8
+DIRECTIONS = [(1,1),(1,0),(1,-1),(0,1),(0,-1),(-1,1),(-1,0),(-1,-1)]
+
+
 
 class Player:
     
@@ -15,6 +19,69 @@ class Player:
         also timed.
         """
         pass
+
+    # check if a column is full
+    def column_is_full(self, col, board: np.ndarray):
+        return board[0][col] != 0
+
+
+    # check the maximum connects of a given direction
+    # may be used for primary herustic
+    # player = +1 or -1
+    def check_direction(self, player, c, r, dc, dr, board: np.ndarray):
+        for i in range(5):
+            # check if out of bound or is opponent/empty
+            if r+dr*i > rows or r+dr*i < 0 or c+dc*i > cols or c+dc*i < 0 or board[r+dr*i][c+dc*i] != player:
+                return i
+        return 5
+
+    # check the maximum connects of a given direction (and count non-fill grid as connected)
+    # may be used for alternate herustic
+    # player = +1 or -1
+    def check_direction_lenient(self, player, c, r, dc, dr, board: np.ndarray):
+        for i in range(5):
+            # check if out of bound or is opponent
+            if r+dr*i > rows or r+dr*i < 0 or c+dc*i > cols or c+dc*i < 0 or board[r+dr*i][c+dc*i] != 0-player:
+                return i
+        return 5
+
+    # count all connects of a given player
+    # player = +1 or -1
+    # return: an array with index 0 to 5 with count
+    def count_connect(self,player,board: np.ndarray):
+        result = [0,0,0,0,0,0]
+        for i in range(rols):
+            for j in range(cols):
+                # do not need to check all 8 directions
+                for dc,dr in DIRECTIONS[:4]:
+                    ++result[check_direction(player,i,j,dc,dr,board)]
+        # adjust result
+        # ****************************** WARNING *********************************
+        # * CODE FOR ADJUSTING LENIENT VERSION (IF ANY) NEED TO BE DIFFERENT !!! *
+        # ************************************************************************
+        # a 5-connect will produce 1 extra 1,2,3,4, 4-connect produce 1 extra 1,2,3,
+        # 3-connect produce 1 extra 1,2, and 2-connect produce 1 extra 1
+        for i in [5,4,3,2]:
+            for j in range(1,i):
+                result[j] -= result[i]
+
+        return result
+
+    # count all connects of a given player
+    # may be used for alternate herustic
+    # player = +1 or -1
+    # return: an array with index 0 to 5 with count
+    def count_connect_lenient(self,player,board: np.ndarray):
+        result = [0,0,0,0,0,0]
+        for i in range(rols):
+            for j in range(cols):
+                # do not need to check all 8 directions
+                for dc,dr in DIRECTIONS[:4]:
+                    ++result[check_direction_lenient(player,i,j,dc,dr,board)]
+
+        return result
+
+
 
 
     def play(self, board: np.ndarray):
@@ -41,6 +108,7 @@ class Player:
         -------
         integer corresponding to the column of the board where you want to drop your disc.
         """
+
         raise NotImplementedError()
 
 
